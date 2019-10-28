2420ceba3eeaebc5e780e18792fd4aa82e82d509
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:17 -0500

    freeclimb code changes

--- getAQueue.js
+++ getAQueue.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const persephonySDK = require('@persephony/sdk')
+const freeclimbSDK = require('@freeclimb/sdk')
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-const persephony = persephonySDK(accountId, authToken)
+const freeclimb = freeclimbSDK(accountId, authToken)
 
 // Retreive a queue by queueId
-persephony.api.queues.get(queueId).then(queue => {
+freeclimb.api.queues.get(queueId).then(queue => {
   // Use the queue object
 }).catch(err => {/** Catch Errors */ })
