ea85d59aed4349dd53425a0e0629fea7f6988112
Logan Underwood <lunderwood@vailsys.com>
2024-10-18 13:32:14 -0500

    freeclimbsdk updates

--- getAQueue.js
+++ getAQueue.js
@@ -1,10 +1,22 @@
-require('dotenv').config()
-const freeclimbSDK = require('@freeclimb/sdk')
-const accountId = process.env.ACCOUNT_ID
-const apiKey = process.env.API_KEY
-const freeclimb = freeclimbSDK(accountId, apiKey)
+require("dotenv").config();
+const freeclimbSDK = require("@freeclimb/sdk");
+
+const accountId = process.env.ACCOUNT_ID;
+const apiKey = process.env.API_KEY;
+const queueId = "your-queue-id";
+
+const freeClimbConfig = freeclimbSDK.createConfiguration({
+  accountId,
+  apiKey,
+});
+const apiInstance = new freeclimbSDK.DefaultApi(freeClimbConfig);
 
 // Retreive a queue by queueId
-freeclimb.api.queues.get(queueId).then(queue => {
-  // Use the queue object
-}).catch(err => {/** Catch Errors */ })
+apiInstance
+  .getAQueue(queueId)
+  .then((queue) => {
+    // Use the queue object
+  })
+  .catch((err) => {
+    /** Catch Errors */
+  });
